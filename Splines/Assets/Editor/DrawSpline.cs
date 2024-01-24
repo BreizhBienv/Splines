97a8406a8e676f356dd3e85cc3228c3a1712ad85
Gauthier Bienvenu <BreizhBienvenu@gmail.com>
2024-01-24 16:21:04 +0100

    Display curve

--- Splines/Assets/Editor/DrawSpline.cs
+++ Splines/Assets/Editor/DrawSpline.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
 [CustomEditor(typeof(Spline))]
 public class DrawSpline : Editor
 {
+    private const int lineSteps = 100;
+
     void OnSceneGUI()
     {
         Spline be = target as Spline;
@@ -15,13 +15,32 @@ public class DrawSpline : Editor
         be.m_SplinePoints[2] = Handles.PositionHandle(be.m_SplinePoints[2], Quaternion.identity);
         be.m_SplinePoints[3] = Handles.PositionHandle(be.m_SplinePoints[3], Quaternion.identity);
 
+        Handles.color = Color.red;
+        Handles.SphereHandleCap(0, be.m_SplinePoints[0], Quaternion.identity, 1, EventType.Repaint);
+        Handles.color = Color.yellow;
+        Handles.SphereHandleCap(0, be.m_SplinePoints[1], Quaternion.identity, 1, EventType.Repaint);
+        Handles.SphereHandleCap(0, be.m_SplinePoints[2], Quaternion.identity, 1, EventType.Repaint);
+        Handles.color = Color.green;
+        Handles.SphereHandleCap(0, be.m_SplinePoints[3], Quaternion.identity, 1, EventType.Repaint);
+
         // Visualize the tangent lines
+        Handles.color = Color.gray;
         Handles.DrawDottedLine(be.m_SplinePoints[0], be.m_SplinePoints[1], 5);
         Handles.DrawDottedLine(be.m_SplinePoints[3], be.m_SplinePoints[2], 5);
 
-        Handles.DrawBezier(
-            be.m_SplinePoints[0], be.m_SplinePoints[3],
-            be.m_SplinePoints[1], be.m_SplinePoints[2],
-            Color.red, null, 5f);
+        Handles.color = Color.red;
+        Vector3 lineStart = be.GetPoint(0f);
+        Vector3 lineFinish = be.GetPoint(1f);
+        for (int i = 1; i <= lineSteps; i++)
+        {
+            Vector3 lineEnd = be.GetPoint(i / (float)lineSteps);
+            Handles.DrawLine(lineStart, lineEnd, 2f);
+            lineStart = lineEnd;
+        }
+
+        //Handles.DrawBezier(
+        //    be.m_SplinePoints[0], be.m_SplinePoints[3],
+        //    be.m_SplinePoints[1], be.m_SplinePoints[2],
+        //    Color.red, null, 5f);
     }
 }
--- Splines/Assets/Script/Spline.cs
+++ Splines/Assets/Script/Spline.cs
@@ -7,34 +7,41 @@ public class Spline : MonoBehaviour
     [SerializeField] public ECurveType m_CurveType;
 
     [SerializeField] private GameObject m_MovingGO;
-    
+
     private float m_Tvalue;
 
     private void Update()
     {
+        m_MovingGO.transform.position = GetPoint(m_Tvalue);
+
         m_Tvalue = (m_Tvalue + Time.deltaTime) % 1f;
+    }
+
+    public Vector3 GetPoint(float pT)
+    {
         Vector3 newPos = Vector3.zero;
+        float t = Mathf.Clamp01(pT);
 
         switch (m_CurveType)
         {
             default:
             case ECurveType.Bézier:
-                newPos = SplinesHelper.ComputeBézierCurve(m_Tvalue, m_SplinePoints);
+                newPos = SplinesHelper.ComputeBézierCurve(t, m_SplinePoints);
                 break;
 
             case ECurveType.Hermite:
-                newPos = SplinesHelper.ComputeHermite(m_Tvalue, m_SplinePoints);
+                newPos = SplinesHelper.ComputeHermite(t, m_SplinePoints);
                 break;
 
             case ECurveType.B_Spline:
-                newPos = SplinesHelper.ComputeBSplineCurve(m_Tvalue, m_SplinePoints);
+                newPos = SplinesHelper.ComputeBSplineCurve(t, m_SplinePoints);
                 break;
 
             case ECurveType.Catmull_Rom:
-                newPos = SplinesHelper.ComputeCatmullRomCurve(m_Tvalue, m_SplinePoints);
+                newPos = SplinesHelper.ComputeCatmullRomCurve(t, m_SplinePoints);
                 break;
         }
 
-        m_MovingGO.transform.position = newPos;
+        return newPos;
     }
 }
